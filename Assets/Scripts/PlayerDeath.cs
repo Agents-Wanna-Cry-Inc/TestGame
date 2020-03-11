2532ae6f82d8bc307b05913b4e49235fd467848e
Mervin <m.dejong26@students.uu.nl>
2020-03-11 11:20:51 +0100

    Added basic respawning/fall death

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public Collider2D trigger;
+    public Transform spawnPoint;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other == trigger)
+        {
+            this.transform.position = spawnPoint.transform.position;
+        }
+    }
+}
