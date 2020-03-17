d6432cba4c61521d302aca1c33f150828fc99016
Mervin <m.dejong26@students.uu.nl>
2020-03-17 16:33:37 +0100

    Updated level assets
    Added more floorboard tiles.
    Created a 16x16 version of the floorboards for better proportions.
    Objects despawn when falling out of the level.

---
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    public Collider2D trigger;    // The collider that triggers a respawn
+
+    // Fires when colliding with a collider that is flagged as trigger
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // *** Check if the collider that triggered the event is the respawn trigger
+        if (other == trigger)
+        {
+            // *** Destroy the object as it is out of bounds to save memory and prevent floating point overflow
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
-    public Collider2D trigger;
-    public Transform spawnPoint;
+    public Collider2D trigger;      // The collider that triggers a respawn
+    public Transform spawnPoint;    // The spawnpoint of the player
 
+    // Fires when colliding with a collider that is flagged as trigger
     void OnTriggerEnter2D(Collider2D other)
     {
+        // *** Check if the collider that triggered the event is the respawn trigger
         if (other == trigger)
         {
+            // *** Move the player back to the spawnpoint
             this.transform.position = spawnPoint.transform.position;
         }
     }
