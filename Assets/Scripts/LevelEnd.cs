63ed2bcf00ae402fee2509663f15ff47c257963f
Mervin <m.dejong26@students.uu.nl>
2020-03-18 18:29:06 +0100

    Added scene transitions

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+    [SerializeField] public Collider2D playerCollider;
+    [SerializeField] public Animator transition;
+    [SerializeField] public float transitionDuration;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider == playerCollider)
+        {
+            StartCoroutine(LoadNextLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        }
+    }
+
+    IEnumerator LoadNextLevel(int levelIndex)
+    {
+        transition.SetTrigger("StartTransition");
+
+        yield return new WaitForSeconds(transitionDuration);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
